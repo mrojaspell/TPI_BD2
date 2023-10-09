0453d06c6bc862b5fcb52ec919882c8f15bbb081
MSGronda <mgronda@itba.edu.ar>
2023-10-09 16:56:30 -0300

    feat: postgres migration
    Co-Authored-By: MarcosCasiraghi <86983868+MarcosCasiraghi@users.noreply.github.com>
    Co-Authored-By: Máximo Rojas Pelliccia <86984301+mrojaspell@users.noreply.github.com>

---
+++ migration/migration.js
@@ -0,0 +1,96 @@
+const Pool = require("pg").Pool;
+
+async function  migration(){
+    const pool = new Pool({
+        "user": "postgres",
+        "password": "basededatos2",
+        "host": "localhost",
+        "port": "5432",
+        "database": "postgres"
+    })
+
+    // Obtenemos los clientes
+
+    const clientMap = {}
+    const clientInfo = await pool.query(
+        "SELECT * FROM e01_cliente c LEFT JOIN e01_telefono t ON c.nro_cliente = t.nro_cliente"
+    )
+    for(let info of clientInfo.rows){
+        if( ! Object.hasOwn(clientMap, info['nro_cliente'])){
+            clientMap[info['nro_cliente']] = {
+                "nro_cliente": info['nro_cliente'],
+                "nombre": info['nombre'],
+                "apellido": info['apellido'],
+                "direccion": info['direccion'],
+                "activo": info['activo'],
+                'telefonos': []
+            }
+
+            if(info['nro_telefono'] !== null){
+                clientMap[info['nro_cliente']]['telefonos'].push({
+                    'codigo_area': info['codigo_area'],
+                    'nro_telefono': info['nro_telefono'],
+                    'tipo': info['tipo']
+                })
+            }
+
+        }else{
+            if(info['nro_telefono'] !== null){
+                clientMap[info['nro_cliente']]['telefonos'].push({
+                    'codigo_area': info['codigo_area'],
+                    'nro_telefono': info['nro_telefono'],
+                    'tipo': info['tipo']
+                })
+            }
+        }
+    }
+    const clientList = []
+    clientList.push(...Object.values(clientMap))
+
+   // Obtenemos las facturas
+    const billMap = {}
+    const billInfo = await pool.query(
+        "SELECT * FROM e01_factura f LEFT JOIN e01_detalle_factura d on f.nro_factura = d.nro_factura"
+    )
+    for(let info of billInfo.rows){
+        if( ! Object.hasOwn(billMap, info['nro_factura'])){
+            billMap[info['nro_factura']] = {
+                "nro_factura": info['nro_factura'],
+                "fecha": info['fecha'],
+                "total_sin_iva": info['total_sin_iva'],
+                "iva": info['iva'],
+                "total_con_iva": info['total_con_iva'],
+                'detalles': []
+            }
+            if(info['nro_item'] !== null){
+                billMap[info['nro_factura']]['detalles'].push({
+                    'nro_item': info['nro_item'],
+                    'cantidad': info['cantidad'],
+                    'codigo_producto': info['codigo_producto']
+                })
+            }
+        }else{
+            billMap[info['nro_factura']]['detalles'].push({
+                'nro_item': info['nro_item'],
+                'cantidad': info['cantidad'],
+                'codigo_producto': info['codigo_producto']
+            })
+        }
+    }
+    const billList = []
+    billList.push(...Object.values(billMap))
+    let accum = 0
+    for(let elem of billList){
+        accum += elem['detalles'].length
+    }
+
+    // Obtenemos los productos
+    const productInfo = await pool.query(
+        "SELECT * FROM e01_producto"
+    )
+    const productList = productInfo.rows
+}
+
+(async () => {
+    await migration();
+})();
