398819357c2373c9fc33955ce7f4029f8009eeb0
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-10-25 18:07:08 -0300

    feat: mongo api, delete and post clients
    Co-Authored-By: Máximo Rojas Pelliccia <86984301+mrojaspell@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -18,6 +18,9 @@ const ClientCodes = {
     InvalidAddressType: [6, 'Tipo invalido de: direccion. Tipo esperado: string.'],
     InvalidAddressLength: [7, 'Longitud invalida de: direccion. Longitud maxima: 44.'],
     InvalidActiveType: [8, 'Tipo invalido de: activo. Tipo esperado: integer {0, 1}.'],
+    InvalidTelephoneArray: [10, 'Tipo invalido de: telefonos. Tipo esperado: arreglo de objetos.'],
+    InvalidTelephoneContact: [11, 'Tipo invalido de: telefono. Tipo esperado: objeto con codigo_area, num_telefono, tipo.'],
+    InvalidTelephoneContactType: [12, 'Tipo invalido de: dato de contacto. Tipo esperado: codigo_area: integer, num_telefono: integer, tipo: char.'],
     Valid: [9, 'Entrada valida.']
 }
 const ProductCodes = {
@@ -61,6 +64,46 @@ function validate_client(body){
     }
     if(typeof(body['activo']) !== 'number' || (body['activo'] !== 0 && body['activo'] !== 1)){
         return ClientCodes.InvalidActiveType;
+    }
+    if(config["database"] === "mongodb"){
+        if (body['telefonos'] === undefined) {
+            // Handle the error for missing 'telefonos' field
+            return ClientCodes.InvalidTelephoneArray;
+        } else {
+            // Check if 'telefonos' is an array
+            if (!Array.isArray(body['telefonos'])) {
+                // Handle the error for 'telefonos' not being an array
+                return ClientCodes.InvalidTelephoneArray;
+            } else {
+                // Iterate through the 'telefonos' array and validate each object
+                for (const telefono of body['telefonos']) {
+                    if (typeof telefono !== 'object' || telefono === null) {
+                        // Handle the error for an invalid 'telefono' object
+                        return ClientCodes.InvalidTelephoneContact;
+                    }
+                    // Check for the required attributes in each 'telefono' object
+                    if (
+                        telefono['codigo_area'] === undefined ||
+                        telefono['nro_telefono'] === undefined ||
+                        telefono['tipo'] === undefined
+                    ) {
+                        // Handle the error for missing attributes in 'telefono'
+                        return ClientCodes.InvalidTelephoneContact;
+                    }
+                    
+                    // Check data types of the attributes
+                    if (
+                        typeof telefono['codigo_area'] !== 'number' ||
+                        typeof telefono['nro_telefono'] !== 'number' ||
+                        typeof telefono['tipo'] !== 'string' ||
+                        telefono['tipo'].length !== 1
+                    ) {
+                        // Handle the error for invalid data types
+                        return ClientCodes.InvalidTelephoneContactType;
+                    }
+                }
+            }
+        }
     }
     return ClientCodes.Valid;
 }
@@ -156,20 +199,29 @@ app.get('/clientes/:id', async (req, res) => {
 
 // Delete client
 app.delete('/clientes/:id', async (req, res) => {
+    const client = await getClient()
+    const { id } = req.params
     try{
-        const { id } = req.params
-        const resp = await pool.query(
-            'DELETE FROM e01_cliente WHERE nro_cliente = $1', [
-                id
-            ]
-        )
-        if(resp.rowCount === 1){
-            res.status(200).send({respuesta: `Cliente: ${id} eliminado.`})
+        if( config["database"] === "postgres"){
+            const resp = await client.query(
+                'DELETE FROM e01_cliente WHERE nro_cliente = $1', [
+                    id
+                ]
+            )
+            if(resp.rowCount === 1){
+                res.status(200).send({respuesta: `Cliente: ${id} eliminado.`})
+            }
+            else{
+                res.status(400).send({respuesta: `nro_cliente: ${id} es invalido.`})
+            }        
+        }else if( config["database"] === "mongodb"){
+            const resp = await client.collection("cliente").deleteOne({nro_cliente: Number(id)})
+            if( resp["deletedCount"] === 1){
+                res.status(200).send({respuesta: `Cliente: ${id} eliminado.`})
+            }else{
+                res.status(400).send({respuesta: `nro_cliente: ${id} es invalido.`})
+            }
         }
-        else{
-            res.status(400).send({respuesta: `nro_cliente: ${id} es invalido.`})
-        }
-
     } catch (e) {
         console.error(e)
         res.status(400).send({respuesta: e.detail})  // TODO: cambiar esto
@@ -177,19 +229,23 @@ app.delete('/clientes/:id', async (req, res) => {
 })
 // Create client
 app.post('/clientes', async (req, res) => {
+    const client = await getClient()
     try{
         const input_check = validate_client(req.body)
-
         if(input_check === ClientCodes.Valid){
-            await pool.query(
-                'INSERT INTO e01_cliente(nro_cliente, nombre, apellido, direccion, activo) VALUES ($1, $2, $3, $4, $5)',
-                [req.body['nro_cliente'], req.body['nombre'], req.body['apellido'], req.body['direccion'], req.body['activo']]
-            )
-            res.status(200).send({respuesta: req.body})
-        }
-        else{
+            if(config["database"] === "postgres"){
+                await client.query(
+                    'INSERT INTO e01_cliente(nro_cliente, nombre, apellido, direccion, activo) VALUES ($1, $2, $3, $4, $5)',
+                    [req.body['nro_cliente'], req.body['nombre'], req.body['apellido'], req.body['direccion'], req.body['activo']]
+                )
+                res.status(200).send({respuesta: req.body})
+            }else if(config["database"] === "mongodb"){
+                await client.collection("cliente").insertOne(req.body)
+                res.status(200).send({respuesta: req.body})
+            }
+        }else{
             res.status(400).send({respuesta: input_check[MESSAGE]})
-        }
+        }        
     } catch (e) {
         console.error(e)
         res.status(400).send({respuesta: e.detail})  // TODO: cambiar esto
